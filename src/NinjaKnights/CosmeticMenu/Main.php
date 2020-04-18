2621e9fee56643be99608c8482bb264170132634
TheKnightGamerZ <52719312+TheKnightGamerZ@users.noreply.github.com>
2020-04-18 20:10:12 +0300

    Update Main.php

--- src/NinjaKnights/CosmeticMenu/Main.php
+++ src/NinjaKnights/CosmeticMenu/Main.php
@@ -55,40 +55,7 @@ use pocketmine\nbt\tag\DoubleTag;
 use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\EnumTag;
 
-use pocketmine\level\particle\AngryVillagerParticle;
-use pocketmine\level\particle\BlockForceFieldParticle;//
-use pocketmine\level\particle\BubbleParticle;//
-use pocketmine\level\particle\CriticalParticle;//
-use pocketmine\level\particle\DestroyBlockParticle;//
-use pocketmine\level\particle\DustParticle;//
-use pocketmine\level\particle\EnchantParticle;//
-use pocketmine\level\particle\EnchantmentTableParticle;//
-use pocketmine\level\particle\EntityFlameParticle;//
-use pocketmine\level\particle\ExplodeParticle;//
-use pocketmine\level\particle\FlameParticle;
-use pocketmine\level\particle\FloatingTextParticle;//
-use pocketmine\level\particle\GenericParticle;//
-use pocketmine\level\particle\HappyVillagerParticle;
-use pocketmine\level\particle\HeartParticle;
-use pocketmine\level\particle\HugeExplodeParticle;
-use pocketmine\level\particle\HugeExplodeSeedParticle;//
-use pocketmine\level\particle\InkParticle;//
-use pocketmine\level\particle\InstantEnchantParticle;
-use pocketmine\level\particle\ItemBreakParticle;//-//
-use pocketmine\level\particle\LavaDripParticle;
-use pocketmine\level\particle\LavaParticle;//
-use pocketmine\level\particle\MobSpawnParticle;
-use pocketmine\level\particle\Particle;//
-use pocketmine\level\particle\PortalParticle;//
-use pocketmine\level\particle\RainSplashParticle;
-use pocketmine\level\particle\RedstoneParticle;//
-use pocketmine\level\particle\SmokeParticle;
-use pocketmine\level\particle\SnowballPoofParticle;
-use pocketmine\level\particle\SplashParticle;//
-use pocketmine\level\particle\SporeParticle;//
-use pocketmine\level\particle\TerrainParticle;//
-use pocketmine\level\particle\WaterDripParticle;
-use pocketmine\level\particle\WaterParticle;//
+use pocketmine\entity\Skin;
 
 /* Will be used later on
 use pocketmine\level\sound\PopSound;
@@ -1075,6 +1042,7 @@ class Main extends PluginBase implements Listener {
         }
 
     }
+
 
 /*   //This is for Diamond Rain Particle
 	public function onItemSpawn(ItemSpawnEvent $event) {
